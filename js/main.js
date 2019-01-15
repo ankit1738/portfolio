3db7edb9e42383fe86fcf0880f0b1fb9690807c7
unknown <ankit1738@gmail.com>
2019-01-15 22:42:58 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+	$("#modal-button-torsokart").click(function(){
+		$("#modal-torsokart").removeClass("closeModal");
+	});
+
+	$("#close-torsokart").click(function(){
+		$("#modal-torsokart").addClass("closeModal");
+	});
+
+	$("#modal-button-blogweb").click(function(){
+		$("#modal-blogweb").removeClass("closeModal");
+	});
+
+	$("#close-blogweb").click(function(){
+		$("#modal-blogweb").addClass("closeModal");
+	});
+
+	$("#modal-button-webomania").click(function(){
+		$("#modal-webomania").removeClass("closeModal");
+	});
+
+	$("#close-webomania").click(function(){
+		$("#modal-webomania").addClass("closeModal");
+	});
+
+	$("#modal-button-shopapp").click(function(){
+		$("#modal-shopapp").removeClass("closeModal");
+	});
+
+	$("#close-shopapp").click(function(){
+		$("#modal-shopapp").addClass("closeModal");
+	});
+	
+
+
+}); 
